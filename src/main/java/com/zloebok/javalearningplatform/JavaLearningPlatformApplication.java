dc5ad1bdf071bd2a7c9db884f1df4e658afd9f2f
Danila Zavjalovs <danilazavjalovs@gmail.com>
2023-11-28 20:26:44 +0200

    project init

---
+++ src/main/java/com/zloebok/javalearningplatform/JavaLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zloebok.javalearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearningPlatformApplication.class, args);
+    }
+
+}
